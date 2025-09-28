0a989dbdb0af8b558c65e6acdc76db3085eeaa94
MinaNesimEdwar <nessimmina30@gmail.com>
2025-09-28 18:29:54 +0300

    edit animation in cart

--- src/app/[locale]/cart/page.jsx
+++ src/app/[locale]/cart/page.jsx
@@ -1,22 +1,29 @@
-import CartItem from '@/app/components/cartItem'
-import React from 'react'
+import CartItem from "@/app/components/cartItem";
+import React from "react";
+import * as motion from "motion/react-client"; //server
 
 function Cart() {
   return (
-    <div className='container  pb-20 pt-24 lg:pt-32  space-y-12'>
-      <div className=' bg-light-black rounded-2xl p-6 space-y-6'>
-
-     <div className='space-y-4'>
-                <h3 className=' text-main font-bold text-2xl'>Shop Cart</h3>
-        <p className=' text-gray-400'>Total Cart Price: <span className=' font-bold'>9876545</span></p>
-     </div>
-     <CartItem/>
-     <CartItem/>
-     <CartItem/>
-     <CartItem/>
-      </div>
+    <div className="container  pb-20 pt-24 lg:pt-32  space-y-12">
+      <motion.div
+      initial={{ opacity: 0, y: 30 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 1 }}
+      className=" bg-light-black rounded-2xl p-6 space-y-6">
+        <div className="space-y-4">
+          <h3 className=" text-main font-bold text-2xl">Shop Cart</h3>
+          <p className=" text-gray-400">
+            Total Cart Price: <span className=" font-bold">9876545</span>
+          </p>
+        </div>
+        <CartItem />
+        <CartItem />
+        <CartItem />
+        <CartItem />
+      </motion.div>
     </div>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
--- src/app/components/cartItem.jsx
+++ src/app/components/cartItem.jsx
@@ -4,10 +4,16 @@ import React from 'react'
 import { MdDeleteForever } from "react-icons/md";
 import { CiSquarePlus } from "react-icons/ci";
 import { CiSquareMinus } from "react-icons/ci";
+import * as motion from "motion/react-client"; //server
 
 function CartItem() {
   return (
-    <div className=' flex items-center justify-between border-b border-main p-4 bg-white rounded-2xl'>
+    <motion.div
+    initial={{ opacity: 0, scale: 0.8}}
+    whileInView={{ opacity: 1, scale: 1 }}
+    viewport={{ once: true }}
+    transition={{ duration: 1 }}
+    className=' flex max-md:flex-col gap-4  items-center justify-between border-b border-main p-4 bg-white rounded-2xl'>
       <div className=' flex items-center gap-6'>
         <Image src="/product2.png" alt="product" width={100} height={100} />
         <div className=' space-y-2'>
@@ -18,7 +24,7 @@ function CartItem() {
       <div className=' text-2xl flex items-center gap-4'><CiSquarePlus className='text-main size-8 ' />8<CiSquareMinus  className='text-main size-8 ' /></div>
              <Button className={'p-0  text-white border-none cursor-pointer flex items-center gap-2  hover:gap-3 transition-all duration-300 bg-main hover:bg-main'}><MdDeleteForever className=' size-5 text-white'/>Remove</Button>
 
-    </div>
+    </motion.div>
   )
 }
 
